e131a1280563c2c7bd318a719e169ef0f74e6fb2
Alex <csiszaralex@gmail.com>
2021-02-13 01:19:12 +0100

    :green_heart: Update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.route("/").get((req, res) => {
-  res.status(200).send("<h1>This page is perfect</h1>");
+  res.status(200).send("<h1>This page is perfect and update</h1>");
 });
 
 app.listen(3000, () => {
